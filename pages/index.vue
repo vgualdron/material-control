e20879e12a9c31622b7b8779299cee2fe7c13db7
Victor Gualdron Ruiz <victor.gualdron@masivian.com>
2021-05-06 11:22:21 -0500

    Add config pwa

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <section class="section">
       <h3 class="subtitle is-6 has-text-grey">
-        Status:
+        * Status:
         <a v-if="$nuxt.isOffline" href="#">
           IS OFFLINE
         </a>
