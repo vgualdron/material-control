4f4dc14c16267cba4f75c2003b9cddaa6843e23a
Victor Gualdron Ruiz <victor.gualdron@masivian.com>
2021-05-07 16:39:16 -0500

    resolve conflicts

--- plugins/workbox-sync.js
+++ plugins/workbox-sync.js
@@ -1,12 +1,6 @@
 const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin('formQueue', {
   maxRetentionTime: 1 // Retry for max of 24 Hours (specified in minutes)
 });
-<<<<<<< HEAD
-workbox.routing.registerRoute(
-  /https:\/\/jsonplaceholder\.typicode\.com\/posts/,
-  new workbox.strategies.NetworkOnly({
-    plugins: [bgSyncPlugin]
-=======
 
 workbox.routing.registerRoute(
   /https:\/\/jsonplaceholder\.typicode\.com\/posts/,
@@ -14,7 +8,6 @@ workbox.routing.registerRoute(
     plugins: [
       bgSyncPlugin
     ]
->>>>>>> 61cced6b8353627a20622a28d942d0ba3d1ef172
   }),
   'POST'
 );
