08ce1a07f80e51d3c3bd254db8f8bf310222dfa3
Victor Gualdron Ruiz <vgualdron@estratec.local>
2022-03-21 20:28:31 -0500

    test bugfix router

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 import { typesAuth as types } from '@/store/auth/types';
 export default function auth ({ store, redirect, route }) {
+  console.log(route.path);
   if (route.path !== '/login' && !store.getters[types.PATH + '/' + types.getters.IS_LOGGED_IN]) {
     return redirect('/login');
   }
